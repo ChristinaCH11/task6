4177ea8540f3b2a4b62b063306142a1e12a8ab63
Chernomorets <90358344+ChristinaCH11@users.noreply.github.com>
2021-12-28 04:58:37 +0200

    Create index6.js

---
+++ index6.js
@@ -0,0 +1,13 @@
+btn.addEventListener("click",fnc);
+function fnc(){
+    console.log(5.checked);
+    console.log(13.checked);
+    console.log(23.checked);
+    console.log(21.checked);
+    console.log(27.checked);
+    if (23.checked){
+        alert("Правильный ответ!:)");
+    } else {
+        alert("Неправильный ответ:(");
+    }
+}
